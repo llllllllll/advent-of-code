cebd7f54bcaf60e082fbf537df7f0bb57b5cb387
Joe Jevnik <joe@quantopian.com>
2019-12-04 04:39:49 -0500

    directory per language

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((eval . (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)))
+         (eval . (add-to-list 'auto-mode-alist '("\\.uml" . python-mode)))))
+ (python-mode . ((fill-column . 79)))
+ (c++-mode . ((c-basic-offset . 4)
+              (fill-column . 90)
+              (flycheck-gcc-language-standard . "gnu++17")))))
