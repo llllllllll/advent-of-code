8dfd00fed54c423bcd6128ca686aa7aef666cc34
Joe Jevnik <joe@quantopian.com>
2020-12-09 01:13:21 -0500

    don't store iterator in static

--- 2020/09/c++/part-2.cc
+++ 2020/09/c++/part-2.cc
@@ -47,15 +47,8 @@ int first_invalid(I begin, I end) {
     throw std::runtime_error{"no solution"};
 }
 
-template<typename I>
-long find_weakness(long target, long sum, I begin, I end) {
-    struct hash {
-        std::size_t operator()(std::pair<I, I> p) const {
-            return reinterpret_cast<uintptr_t>(&*p.first) ^
-                   reinterpret_cast<uintptr_t>(&*p.second);
-        }
-    };
-    static std::unordered_map<std::pair<I, I>, long, hash> memo;
+template<typename I, typename M>
+long find_weakness(long target, long sum, I begin, I end, M& memo) {
     const auto search = memo.find({begin, end});
     if (search != memo.end()) {
         return search->second;
@@ -66,18 +59,27 @@ long find_weakness(long target, long sum, I begin, I end) {
     if (end - begin < 2 || sum < target) {
         return memo[{begin, end}] = -1;
     }
-    const auto right = find_weakness(target, sum - *(end - 1), begin, end - 1);
+    const auto right = find_weakness(target, sum - *(end - 1), begin, end - 1, memo);
     if (right > 0) {
         return memo[{begin, end}] = right;
     }
-    return memo[{begin, end}] = find_weakness(target, sum - *begin, begin + 1, end);
+    return memo[{begin, end}] = find_weakness(target, sum - *begin, begin + 1, end, memo);
 }
 
 int main() {
     const auto ints = read_ints(std::istream_iterator<aoc::line>{std::cin},
                                 std::istream_iterator<aoc::line>{});
     const auto target = first_invalid(ints.begin(), ints.end());
-    const auto upper = ints.end();
-    const auto sum = std::accumulate(ints.begin(), upper, 0l);
-    std::cout << find_weakness(target, sum, ints.begin(), upper) << '\n';
+    const auto sum = std::accumulate(ints.begin(), ints.end(), 0l);
+
+    using I = decltype(ints.begin());
+    struct hash {
+        std::size_t operator()(std::pair<I, I> p) const {
+            return reinterpret_cast<uintptr_t>(&*p.first) ^
+                reinterpret_cast<uintptr_t>(&*p.second);
+        }
+    };
+
+    std::unordered_map<std::pair<I, I>, long, hash> memo;
+    std::cout << find_weakness(target, sum, ints.begin(), ints.end(), memo) << '\n';
 }
