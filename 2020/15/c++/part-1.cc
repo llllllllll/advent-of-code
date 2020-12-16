6693216d60a6cf028e1d5bc14d64b7d512e9f922
Joe Jevnik <joe@quantopian.com>
2020-12-15 22:58:49 -0500

    use array instead of map

--- 2020/15/c++/part-1.cc
+++ 2020/15/c++/part-1.cc
@@ -11,24 +11,27 @@
 
 #include "aoc/aoc.hpp"
 
+constexpr std::size_t target = 2020;
+
 template<std::ranges::range R>
 auto solution(R&& input) {
-    std::size_t ix = 0;
-    int prev = -1;
-    std::unordered_map<int, int> last_seen;
-    for (const int i : input) {
+    std::vector<int> last_seen(target, -1);
+
+    std::size_t ix = 1;
+    int prev = *input.begin();
+    for (const int i : input | std::views::drop(1)) {
         last_seen[prev] = ix++;
         prev = i;
     }
-    for (; ix < 2020; ++ix) {
-        auto search = last_seen.find(prev);
-        if (search == last_seen.end()) {
+    for (; ix < target; ++ix) {
+        int& search = last_seen[prev];
+        if (search == -1) {
             last_seen[prev] = ix;
             prev = 0;
         }
         else {
-            prev = ix - search->second;
-            search->second = ix;
+            prev = ix - search;
+            search = ix;
         }
     }
     return prev;
