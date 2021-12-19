930fb9b82b4024358aecabd729da3130b550e934
Joe Jevnik <joe@quantopian.com>
2021-12-19 15:34:28 -0500

    2021-10

--- 2021/10/c++/part-1.cc
+++ 2021/10/c++/part-1.cc
@@ -1,10 +1,56 @@
 #include "aoc/aoc.hpp"
 
-template<std::ranges::input_range R>
-auto solution(R&& input) {
+int score(std::string_view cs) {
+    constexpr auto closes = [] {
+        std::array<char, 256> closes;
+        closes[')'] = '(';
+        closes[']'] = '[';
+        closes['}'] = '{';
+        closes['>'] = '<';
+        return closes;
+    }();
+
+    constexpr auto scores = [] {
+        std::array<int, 256> scores;
+        scores[')'] = 3;
+        scores[']'] = 57;
+        scores['}'] = 1197;
+        scores['>'] = 25137;
+        return scores;
+    }();
+
+    std::vector<char> stack;
+    for (char c : cs) {
+        switch (c) {
+        case '(':
+        case '[':
+        case '{':
+        case '<':
+            stack.push_back(c);
+            break;
+        case ')':
+        case ']':
+        case '}':
+        case '>':
+            if (stack.back() != closes[c]) {
+                return scores[c];
+            }
+            stack.pop_back();
+            break;
+        default:
+            aoc::no_solution();
+        }
+    }
     return 0;
 }
 
-int main() {
-    aoc::dbg(solution(aoc::input_range<aoc::line>()));
+template<std::ranges::input_range R>
+auto solution(R&& input) {
+    int sum = 0;
+    for (auto score : input | std::views::transform(score)) {
+        sum += score;
+    }
+    return sum;
 }
+
+int main() { aoc::dbg(solution(aoc::input_range<aoc::line>())); }
--- 2021/10/c++/part-2.cc
+++ 2021/10/c++/part-2.cc
@@ -1,10 +1,65 @@
 #include "aoc/aoc.hpp"
 
-template<std::ranges::input_range R>
-auto solution(R&& input) {
-    return 0;
+long score(std::string_view cs) {
+    constexpr auto closes = [] {
+        std::array<char, 256> closes;
+        closes[')'] = '(';
+        closes[']'] = '[';
+        closes['}'] = '{';
+        closes['>'] = '<';
+        return closes;
+    }();
+
+    constexpr auto scores = [] {
+        std::array<long, 256> scores;
+        scores['('] = 1;
+        scores['['] = 2;
+        scores['{'] = 3;
+        scores['<'] = 4;
+        return scores;
+    }();
+
+    std::vector<char> stack;
+    for (char c : cs) {
+        switch (c) {
+        case '(':
+        case '[':
+        case '{':
+        case '<':
+            stack.push_back(c);
+            break;
+        case ')':
+        case ']':
+        case '}':
+        case '>':
+            if (stack.back() != closes[c]) {
+                return std::numeric_limits<long>::max();
+            }
+            stack.pop_back();
+            break;
+        default:
+            aoc::no_solution();
+        }
+    }
+
+    long score = 0;
+    for (char c : stack | std::views::reverse) {
+        score *= 5;
+        score += scores[c];
+    }
+    return score;
 }
 
-int main() {
-    aoc::dbg(solution(aoc::input_range<aoc::line>()));
+template<std::ranges::input_range R>
+auto solution(R&& input) {
+    std::vector<long> scores;
+    for (auto score : input | std::views::transform(score)) {
+        if (score != std::numeric_limits<long>::max()) {
+            scores.push_back(score);
+        }
+    }
+    std::sort(scores.begin(), scores.end());
+    return scores[scores.size() / 2];
 }
+
+int main() { aoc::dbg(solution(aoc::input_range<aoc::line>())); }
