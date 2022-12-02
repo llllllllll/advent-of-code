f037ceb94f962e83036f7b6114cf02849a65df43
Joe Jevnik <joe@quantopian.com>
2022-12-02 01:13:40 -0500

    2022 02

---
+++ 2022/02/c++/part-1.cc
@@ -0,0 +1,75 @@
+#include <compare>
+
+#include "aoc/aoc.hpp"
+
+enum class Throw : uint8_t {
+    rock,
+    paper,
+    scissors,
+};
+
+enum class Opponent : char {
+    rock = 'A',
+    paper = 'B',
+    scissors = 'C',
+};
+
+enum class Response : char {
+    rock = 'X',
+    paper = 'Y',
+    scissors = 'Z',
+};
+
+enum class Result : int {
+    loss = 0,
+    tie = 3,
+    win = 6,
+};
+
+auto result(Opponent a, Response b) -> Result {
+    switch (a) {
+    case Opponent::rock:
+        switch (b) {
+        case Response::rock: return Result::tie;
+        case Response::paper: return Result::win;
+        case Response::scissors: return Result::loss;
+        default: __builtin_unreachable();
+        }
+    case Opponent::paper:
+        switch (b) {
+        case Response::rock: return Result::loss;
+        case Response::paper: return Result::tie;
+        case Response::scissors: return Result::win;
+        default: __builtin_unreachable();
+        }
+    case Opponent::scissors:
+        switch (b) {
+        case Response::rock: return Result::win;
+        case Response::paper: return Result::loss;
+        case Response::scissors: return Result::tie;
+        default: __builtin_unreachable();
+        }
+    default: __builtin_unreachable();
+    }
+}
+
+auto score(Opponent a, Response b) -> int {
+    return (static_cast<char>(b) - static_cast<char>(Response::rock) + 1)
+        + static_cast<int>(result(a, b));
+}
+
+
+template<std::ranges::input_range R>
+auto solution(R&& input) {
+    int total_score = 0;
+    for (auto&& line : input) {
+        Opponent const a{line.get()[0]};
+        Response const b{line.get()[2]};
+        total_score += score(a, b);
+    }
+    return total_score;
+}
+
+int main() {
+    aoc::dbg(solution(aoc::input_range<aoc::line>()));
+}
---
+++ 2022/02/c++/part-2.cc
@@ -0,0 +1,50 @@
+#include "aoc/aoc.hpp"
+
+enum class Throw : int8_t {
+    rock,
+    paper,
+    scissors,
+};
+
+auto parse_throw(char c) -> Throw {
+    return Throw(c - 'A');
+}
+
+auto throw_score(Throw t) -> int {
+    return static_cast<int8_t>(t) + 1;
+}
+
+enum class TargetResult : int8_t {
+    loss = -1,
+    tie = 0,
+    win = +1,
+};
+
+auto parse_target(char c) -> TargetResult {
+    return TargetResult(c - 'Y');
+}
+
+auto target_score(TargetResult r) -> int {
+    return (static_cast<int8_t>(r) + 1) * 3;
+}
+
+auto needed_throw(Throw a, TargetResult r) {
+    return Throw((static_cast<int8_t>(a) + static_cast<int8_t>(r) + 3) % 3);
+}
+
+
+template<std::ranges::input_range R>
+auto solution(R&& input) {
+    int total_score = 0;
+    for (auto&& line : input) {
+        auto const opponent = parse_throw(line.get()[0]);
+        auto const target = parse_target(line.get()[2]);
+        total_score +=
+            throw_score(needed_throw(opponent, target)) + target_score(target);
+    }
+    return total_score;
+}
+
+int main() {
+    aoc::dbg(solution(aoc::input_range<aoc::line>()));
+}
